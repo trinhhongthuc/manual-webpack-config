8f2e9c29d5749f94975ca388de68afcafbb87f71
Thuc Trinh <trinhhongthuc57@gmail.com>
2025-07-19 21:12:28 +0700

    Initial UI

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+function addTodo() {
+  const input = document.getElementById("todo-input");
+  const list = document.getElementById("todo-list");
+  const text = input.value.trim();
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    li.innerHTML = `
+          <span onclick="this.parentElement.classList.toggle('completed')">${text}</span>
+          <button onclick="this.parentElement.remove()">✖</button>
+        `;
+    list.appendChild(li);
+    input.value = "";
+  }
+}
