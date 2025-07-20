a11319efe667c0ad8159a55bd14b7b253bdef56d
Thuc Trinh <trinhhongthuc57@gmail.com>
2025-07-20 08:21:23 +0700

    Add mini extract plugin

--- src/js/main.js
+++ src/js/main.js
@@ -16,4 +16,5 @@ function addTodo() {
   }
 }
 
+
 window.addTodo = addTodo;
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,22 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
   entry: "./src/js/main.js",
   output: {
-    filename: "[name].js",
+    filename: "js/[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   resolve: {
-    extensions: ['.js', '.css', 'scss', '.html']
+    extensions: [".js", ".css", "scss", ".html"],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
         exclude: /node_modules/,
       },
       {
@@ -34,10 +37,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      filename: 'index.[contenthash:12].html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:12].css",
     }),
   ],
   optimization: {
     minimize: true,
-    minimizer: ['...',new CssMinimizerPlugin()],
+    minimizer: ["...", new CssMinimizerPlugin()],
   },
 };
